a439b1a02dc8caa0f42d7f234d68116b3f517450
hadi <hadihe2002@gmail.com>
2024-05-21 01:41:52 +0330

    ADD: complete sensitivity analysis

--- utils/plot_lists.py
+++ utils/plot_lists.py
@@ -1,13 +1,12 @@
 import matplotlib.pyplot as plt
 
 
-def plot_lists(list1, title="sensitivity analysis for parameter", xlabel="Parameter Value", ylabel="Objective Function"):
-    iterations = range(1, len(list1)+1)
+def plot_lists(array, title="Sensitivity analysis", xlabel="Parameter Value", ylabel="Objective Function"):
+    iterations = range(1, len(array)+1)
 
-    plt.plot(iterations, list1, label="D [ 2 ]")
+    plt.plot(iterations, array)
 
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.legend()
     plt.show()
