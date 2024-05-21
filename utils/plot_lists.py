6116e2b86f5a7822eb61dd63ec8be86dca12a4de
hadi <hadihe2002@gmail.com>
2024-05-22 00:00:28 +0330

    FIX

--- utils/plot_lists.py
+++ utils/plot_lists.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 
 
-def plot_lists(array, xticks, title="Sensitivity analysis", xlabel="Parameter Value", ylabel="Objective Function", ):
+def plot_lists(array, xticks, yticks, title="Sensitivity analysis", xlabel="Parameter Value", ylabel="Objective Function", ):
     plt.plot(xticks, array, marker='o')
 
     plt.xticks(xticks)
+    plt.yticks(yticks)
 
     plt.title(title)
     plt.xlabel(xlabel)
